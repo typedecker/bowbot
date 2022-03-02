dd4fc1ea7b366a3a8123b92231bd8c4b47fb120b
typedecker <75010865+typedecker@users.noreply.github.com>
2022-03-02 19:31:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member) :
-    global welcome_channels
-    
     if member.dm_channel == None :
         await member.create_dm()
     await member.dm_channel.send('* bows to welcome you *')
@@ -61,9 +59,43 @@ async def on_message(message):
             clean_message = message.clean_content[len('$$bowforme ') : ]
             for mention in message.mentions :
                 clean_message = clean_message.replace('@' + mention.name, '').replace(mention.mention, '')
-                print(mention.mention, mention.name, message.content, message.clean_content, message.content.replace(mention.mention, ''))
+                print(mention.mention, mention.name, message.content, clean_message, clean_message.content.replace(mention.mention, ''))
             await message.channel.send('* ' + message.author.name + ' bows to ' + ' ,  '.join([k.mention for k in message.mentions]) + ' ' + clean_message + ' *')
         else :
             await message.channel.send('* ' + message.author.name + ' bows ' + message.content[len('$$bowforme ') : ] + ' *')
+    if message.content.startswith('$$bowbot promote ') and message.author == message.guild.owner :
+        content = message.content
+        target = message.mentions[0]
+        promote_role = message.role_mentions[0]
+        target_roles = [] + target.roles.copy()
+        if not target_roles.__contains__(promote_role) :
+            await message.mentions[0].add_roles(promote_role)
+            await message.channel.send('Congratulations ' + target.name + '! You have been promoted to ' + promote_role.name)
+            if target.dm_channel == None :
+                await target.create_dm()
+            await target.dm_channel.send('Congratulations ' + target.name + '! You have been promoted to ' + promote_role.name)
+        else :
+            await message.channel.send('The member already has that role.')
+    if message.content.startswith('$$bowbot demote ') and message.author == message.guild.owner :
+        content = message.content
+        target = message.mentions[0]
+        # role_name = content[content.index('[') + 1 : content.index(']')]
+        demote_role = message.role_mentions[0]
+        # demote_role = None
+        # for role in message.guild.roles :
+        #     if role.name == role_name :
+        #         demote_role = role
+        #         break
+        target_roles = [] + target.roles.copy()
+        if target_roles.__contains__(demote_role) :
+            # target_roles.remove(demote_role)
+            # message.mentions[0].edit(role = target_roles)
+            await message.mentions[0].remove_roles(demote_role)
+            await message.channel.send('So unfortunate ' + target.name + '! You have been demoted from ' + demote_role.name)
+            if target.dm_channel == None :
+                await target.create_dm()
+            await target.dm_channel.send('So unfortunate ' + target.name + '! You have been demoted from ' + demote_role.name)
+        else :
+            await message.channel.send('The member already does not have that role.')
 
 client.run(os.environ['BOT_TOKEN'])

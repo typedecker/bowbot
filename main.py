ec0e0785a2b6f85bd7b6f208b3086aec05b60685
typedecker <75010865+typedecker@users.noreply.github.com>
2022-03-02 14:55:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import discord, os
 client = discord.Client()
 
 @client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    client.loop.create_task(tasks_loop())
+    
+    game = discord.Game("CWL for TYPE CREW in Clash Of Clans..Hehe ;)")
+    await client.change_presence(activity = game)
+    return
+
+@client.event
 async def on_message(message):
     if message.author == client.user :
         return

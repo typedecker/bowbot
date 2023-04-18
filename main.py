4fe2f6c1aef23e4e031f64b1e8f95c17ebbe5b1f
typedecker <75010865+typedecker@users.noreply.github.com>
2023-04-18 20:34:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import discord
 
 while __name__ == '__main__':
     try:
+        bot_rewrite.keep_alive()
         bot_rewrite.client.run(os.environ['BOT_TOKEN'])
+        bot_rewrite.run()
     except discord.errors.HTTPException as e:
         print(e)
         print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
